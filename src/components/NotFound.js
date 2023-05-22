081a168800177a0a50a92e3b15f33e7000c79b09
ha-rsh20 <20it033@charusat.edu.in>
2023-05-22 12:54:22 +0530

    material ui, applying css and js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,28 @@
+import { Button, ThemeProvider } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { theme } from "../mui_style";
 
 function NotFound() {
-  return <div>Page not found!</div>;
+  const Navigate = useNavigate();
+
+  function toHome() {
+    let confirm = window.confirm("Do you want to navigate to home?");
+    if (confirm) {
+      Navigate("/");
+    }
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        Page not found!
+        <br />
+        <Button variant="contained" onClick={toHome}>
+          Go back to home
+        </Button>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default NotFound;
